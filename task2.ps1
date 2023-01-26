f05e915de6ed982d55606123c59e849cc3e6b077
yulyangi <sailorsmollett@gmail.com>
2023-01-26 14:38:46 +0300

    Edit task2.ps1

--- task2.ps1
+++ task2.ps1
@@ -16,10 +16,10 @@ foreach($item in $file){
    location_id=$item.location_id;
    name=( Get-Culture ).TextInfo.ToTitleCase( $item.Name.ToLower() );
    title=$item.title;
-   email= $fullname[0][0]+"."+$fullname[1]+"@abc.com";
+   email= $fullname[0][0]+"."+$fullname[1]+$item.location_id+"@abc.com";
    department=$item.department}
    }
 
 # write to new file without qoutes
 $new_file | ConvertTo-CSV -NoTypeInformation | 
-% { $_ -Replace '"', ""} | Out-File .\new_accounts.csv -fo -en ascii
+% { $_ -Replace '"', ""} | Out-File .\new_accounts.csv
